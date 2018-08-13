5fc44e7aa7d768477f6a470425012bf2b56648ef
Iuri de Moura Carneiro <iuri.carneiro@cgu.gov.br>
2018-08-13 13:09:09 -0300

    Configurando projeto com messenger4j

--- src/main/java/br/gov/cgu/chatboteouvteste/AppConfig.java
+++ src/main/java/br/gov/cgu/chatboteouvteste/AppConfig.java
@@ -1,5 +1,9 @@
 package br.gov.cgu.chatboteouvteste;
 
+import com.github.messenger4j.Messenger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,8 +12,22 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class AppConfig {
 
+    private final Logger logger = LoggerFactory.getLogger(AppConfig.class);
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
     }
+
+
+    @Bean
+    public Messenger messenger(@Value("${messenger4j.pageAccessToken}") String pageAccessToken,
+                               @Value("${messenger4j.appSecret}") final String appSecret,
+                               @Value("${messenger4j.verifyToken}") final String verifyToken) {
+        logger.info("Parâmetros Messenger:");
+        logger.info("messenger4j.pageAccessToken: {}", pageAccessToken);
+        logger.info("messenger4j.appSecret {}", appSecret);
+        logger.info("messenger4j.verifyToken: {}", verifyToken);
+        return Messenger.create(pageAccessToken, appSecret, verifyToken);
+    }
+
 }
