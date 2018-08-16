c24a7f1b55d1d6e3535133667c107ed011c55146
Iuri de Moura Carneiro <iuri.carneiro@cgu.gov.br>
2018-08-16 15:01:04 -0300

    Configurando projeto com messenger4j

--- src/main/java/br/gov/cgu/chatboteouvteste/aplicacao/GerenciadorDeMensagem.java
+++ src/main/java/br/gov/cgu/chatboteouvteste/aplicacao/GerenciadorDeMensagem.java
@@ -1,8 +1,11 @@
 package br.gov.cgu.chatboteouvteste.aplicacao;
 
+import br.gov.cgu.chatboteouvteste.negocio.EventoUsuario;
 import br.gov.cgu.chatboteouvteste.negocio.MensagemInvalidaException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
@@ -14,15 +17,25 @@ import static br.gov.cgu.chatboteouvteste.Constantes.OBJETO_PAGINA;
 @Service
 public class GerenciadorDeMensagem {
 
+    private final Logger logger = LoggerFactory.getLogger(GerenciadorDeMensagem.class);
+
     private IntegracaoFacebookService facebookService;
+    private EventoUsuario eventoUsuario;
 
     @Autowired
-    public GerenciadorDeMensagem(IntegracaoFacebookService facebookService) {
+    public GerenciadorDeMensagem(IntegracaoFacebookService facebookService, EventoUsuario eventoUsuario) {
         this.facebookService = facebookService;
+        this.eventoUsuario = eventoUsuario;
     }
 
     public void processarMensagem(String mensagem) {
         try {
+logger.debug("Antes atualizacao: {}", eventoUsuario.toString());
+if (StringUtils.isEmpty(eventoUsuario.getSenderId())) {
+    eventoUsuario.setSenderId("XPTO");
+    eventoUsuario.setRecipientId("ABCD");
+}
+logger.debug("Depois atualizacao: {}", eventoUsuario.toString());
             ObjectMapper mapper = new ObjectMapper();
             RequisicaoMensagemDTO requisicaoMensagemDTO = mapper.readValue(mensagem, RequisicaoMensagemDTO.class);
             validarMensagem(requisicaoMensagemDTO);
